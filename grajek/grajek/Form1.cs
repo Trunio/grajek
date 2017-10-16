dd407e23c538d004179164052dc85a85ba698fda
Trunio <32064409+Trunio@users.noreply.github.com>
2017-10-16 14:38:21 +0200

    Add files via upload

---
+++ grajek/grajek/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer grajek = new WindowsMediaPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            grajek.URL = "plecak.mp3";
+            grajek.controls.play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            grajek.controls.pause();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            grajek.controls.stop();
+        }
+    }
+}
